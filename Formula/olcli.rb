2068a7dbcb88b78fd34acdde5a9328001bffb277
Alexander Loth <home@alexloth.com>
2026-02-02 09:16:44 +0100

    Update olcli formula with release sha256

--- Formula/olcli.rb
+++ Formula/olcli.rb
@@ -2,7 +2,7 @@ class Olcli < Formula
   desc "Overleaf CLI - sync LaTeX projects from the command line"
   homepage "https://github.com/aloth/olcli"
   url "https://github.com/aloth/olcli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256_HASH"
+  sha256 "aa0829c06d0ce182f6b05f7d6c0b5efe564df554cc08334d7d5f88d704021bcc"
   license "MIT"
 
   depends_on "node"
