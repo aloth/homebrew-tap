f668d1f7547789d9925fe735d3b47cbcaf9cc798
Alexander Loth <home@alexloth.com>
2026-02-02 11:09:41 +0100

    Update olcli to 0.1.2

--- Formula/olcli.rb
+++ Formula/olcli.rb
@@ -1,8 +1,8 @@
 class Olcli < Formula
   desc "Overleaf CLI - sync LaTeX projects from the command line"
   homepage "https://github.com/aloth/olcli"
-  url "https://github.com/aloth/olcli/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "0175b52b5b9fed630820802169a1179e6b515ad8b331526e411771d8e59a219b"
+  url "https://github.com/aloth/olcli/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "5fda1c9b389c314013026313ae5752168df6c58454e158213b0a85edbf440566"
   license "MIT"
 
   depends_on "node"
