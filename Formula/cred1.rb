493dade24f60dba86c694bfb80866326fd1b4f2f
Alexander Loth <home@alexloth.com>
2026-08-25 17:41:50 +0200

    cred1: bump to 2026-08-25

--- Formula/cred1.rb
+++ Formula/cred1.rb
@@ -1,8 +1,8 @@
 class Cred1 < Formula
   desc "CRED-1 domain credibility checker - library and CLI"
   homepage "https://github.com/aloth/cred-1"
-  url "https://github.com/aloth/cred-1/archive/refs/tags/v2026-08-18.tar.gz"
-  sha256 "e99b180c1337cd77f5df4aa5cf7c459282c9840017586c986edb7924518063ae"
+  url "https://github.com/aloth/cred-1/archive/refs/tags/v2026-08-25.tar.gz"
+  sha256 "a45d0e03d44cf612298aa92c90ae512c10a4c57a8dbf25197944e38519a0ca3c"
   license "MIT"
 
   depends_on "node"
